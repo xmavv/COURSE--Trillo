82d9df1e0d299f940005cddfbe94a037d69c88a0
xmavv <xxmatheooo@gmail.com>
2023-10-29 19:27:09 +0100

    final touches, search bar functionality

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-let chatIcon = document.querySelectorAll('.user-nav__icon-box');
-let chatBox = document.querySelector('.chat');
-let closeIcon = document.querySelector('.chat__head-close');
-let minIcon = document.querySelector('.chat__head-min');
-let maxIcon = document.querySelector('.chat__head-max');
-let chatContent = document.querySelector('.chat__content');
-let chatInput = document.querySelector('.chat__input-box');
+const closeIcon = document.querySelector('.chat__head-close');
+const chatIcon = document.querySelectorAll('.user-nav__icon-box');
+const minIcon = document.querySelector('.chat__head-min');
+const input = document.querySelector('.input-width');
+const chatBox = document.querySelector('.chat');
 
 let counter = 0;
+
+// chat functionality
 
 chatIcon[1].addEventListener('click', () => {
     chatBox.classList.toggle("d-none");
@@ -17,6 +17,9 @@ closeIcon.addEventListener('click', () => {
 })
 
 minIcon.addEventListener('click', () => {
+    const chatContent = document.querySelector('.chat__content');
+    const chatInput = document.querySelector('.chat__input-box');
+
     counter++;
     console.log(counter);
     chatContent.classList.toggle('d-none');
@@ -30,3 +33,55 @@ minIcon.addEventListener('click', () => {
         chatBox.style.height = '5rem';
     }
 })
+
+// input suggestions display
+
+const inputCard = document.querySelectorAll('.input__card-item');
+
+const toggleSuggestions = e => {
+    const value = e.target.value.toLowerCase().replace(/\s/g, '');
+                                                            // "\s to ze bialy znak"
+                                                            // /poczatek i koniec/
+                                                            // /g globalnie nie tylko pierwszy
+                                                            // drugi argument czym zastepujemy
+    inputCard.forEach(t => {
+        const isVisible = t.innerText.toLowerCase().replace(/\s/g, '').includes(value);
+                                                            // dajemy replace tu i tu wowczas
+                                                            // mozemy wpisac ze spacja i bez
+                                                            // i zawsze nam wyszuka
+                                                            // sam na to wpadlem i tak jestem
+                                                            // dumny z siebie B)
+        t.classList.toggle('d-none', !isVisible);
+        if(value === "") t.classList.add('d-none');
+    })
+}
+
+// w addeventlistenerze zawsze argument w tej funckji to jest przekazanie tego eventu
+// a w zasadzie na czym ten event sie wywolal czyli u nas w inpucie
+input.addEventListener('input', e => {
+    toggleSuggestions(e);
+})
+
+input.addEventListener('focus', e => {
+    toggleSuggestions(e);
+})
+
+inputCard.forEach(e => {
+    e.addEventListener('click', t => {
+        const text = t.target.innerText;
+        input.value = text;
+        inputCard.forEach(m => {
+            m.classList.add('d-none');
+        })
+        //  not really know how in other way
+        //  algorithm is reallly not that good now O(n) = n^2 
+        //  ;/
+    })
+})
+
+// blur => focused no more
+// input.addEventListener('blur', () => {
+//     inputCard.forEach(t => {
+//         t.classList.add('d-none');
+//     })
+// })
--- readme.md
+++ readme.md
@@ -1,12 +1,49 @@
 # I am a master in flexbox now 😎😎😎
+
+This is the second website of [css course](https://www.udemy.com/courseadvanced-css-and-sass/) that i built 
+
+## Table of contents
+- [Video](#video)
+- [How to use](#how-to-use)
+- [Deploying project](#deploying-project)
+- [What I learned](#what-did-i-learn)
+- [What I added](#what-did-i-add)
+- [Author](#author)
+
+## Video
+
+
+
+## How to use
+
+To clone and run this webpage, you need [npm](https://nodejs.org/en/download) and [git](https://git-scm.com/downloads) installed on your local computer
+
+In command line
+
+```
+$ git clone https://github.com/your-nick/COURSE--Natours-Project.git
+
+$ npm install
+```
+
+## Deploying project
+
+Final webpage is available at [Trillo]()
+
+## What did i learn
+
 - svg (different svg in one file, so only 1 http request)
 - margin: auto; in flexbox
 - currentColor
 - masks: mask-image, mask-size (as bg)
 
-## Whad did I add
+## What did I add
+
 - captions and effects on gallery
 - chatbox interactions
-- 
-- 
-- 
+- searching suggestions
+
+## Author
+
+- my github profile - [xmavv](https://github.com/xmavv)
+- my behance profile - [mav](https://www.behance.net/mavrgb)
